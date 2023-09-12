90f9a17b67ef862c37e419840fdb310b6f29fc58
safwan-ab <109526410+safwan-ab@users.noreply.github.com>
2023-09-12 10:02:07 +0530

    Add files via upload

---
+++ app/precache-manifest.d11349ded3da21bc778af593f9d26a70.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4fd39268dbc343236cf6202a9c8f5b",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "8b44469b346c820a1533",
+    "url": "/app/static/css/3.849a7a81.chunk.css"
+  },
+  {
+    "revision": "152aca76368bf42f0643",
+    "url": "/app/static/js/0.c41076ac.chunk.js"
+  },
+  {
+    "revision": "8733349dee1a5169f9848eb508ea1569",
+    "url": "/app/static/js/0.c41076ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b44469b346c820a1533",
+    "url": "/app/static/js/3.7016a012.chunk.js"
+  },
+  {
+    "revision": "6d59b312df6986cc2946938ec2575b8a",
+    "url": "/app/static/js/3.7016a012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c72a499e79eaf74c7ce",
+    "url": "/app/static/js/4.06285eb1.chunk.js"
+  },
+  {
+    "revision": "e7ef3db86d3638de8c9e",
+    "url": "/app/static/js/5.92c3599a.chunk.js"
+  },
+  {
+    "revision": "fcc48116bed8e93a920f",
+    "url": "/app/static/js/6.6b91313c.chunk.js"
+  },
+  {
+    "revision": "89e96ad5fc62b78510be",
+    "url": "/app/static/js/7.2d63cb26.chunk.js"
+  },
+  {
+    "revision": "fc408da4c11c44a96c1e",
+    "url": "/app/static/js/8.8b654be9.chunk.js"
+  },
+  {
+    "revision": "9d6d096ffbdbeff731e22f5c1fde238e",
+    "url": "/app/static/js/8.8b654be9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fe0f7053967b3f20b57",
+    "url": "/app/static/js/main.e3c55331.chunk.js"
+  },
+  {
+    "revision": "6cacc1b8a22aef8ebdd7",
+    "url": "/app/static/js/runtime-main.2324e6cf.js"
+  },
+  {
+    "revision": "d9c8e834efe34bcda021069d5532fadc",
+    "url": "/app/static/media/logo.d9c8e834.png"
+  }
+]);
